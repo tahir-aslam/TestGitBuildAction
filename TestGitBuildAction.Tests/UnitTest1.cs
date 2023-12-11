a39edfcd7d51629c812d43118963363ed2d9bde5
tahir-aslam <tahir_aslam786@yahoo.com>
2023-12-11 15:39:09 +0500

    Add project files.

---
+++ TestGitBuildAction.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestGitBuildAction.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
